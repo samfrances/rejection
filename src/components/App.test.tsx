894523ed93891946330b48c3d2e6a3fd63408858
Sam Frances <sam@samfrances.uk>
2020-02-25 20:17:06 +0000

    Linting

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -6,14 +6,14 @@ test("renders rejection app header", () => {
   const { getByTestId } = render(<App />);
   const titleElement = getByTestId("title");
   expect(titleElement).toBeInTheDocument();
-  expect(titleElement.textContent.toLowerCase()).toEqual("rejection app")
+  expect(titleElement.textContent!.toLowerCase()).toEqual("rejection app");
 });
 
 test("renders a form for adding new asks", () => {
   const { container } = render(<App />);
-  const formElement = container.querySelector(`form[data-testid="new-ask-form"]`)
+  const formElement = container.querySelector(`form[data-testid="new-ask-form"]`);
   expect(formElement).toBeInTheDocument();
-})
+});
 
 test("renders the component containing a list of asks", () => {
   const { getByTestId } = render(<App />);
