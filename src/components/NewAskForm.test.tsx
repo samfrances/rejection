c373c25f37692bfb95773a1c5c84130ecf2df138
Sam Frances <sam@samfrances.uk>
2020-02-25 22:52:20 +0000

    Clear ask form after submission

--- src/components/NewAskForm.test.tsx
+++ src/components/NewAskForm.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import NewAskForm from "./NewAskForm";
 
 test("renders an input for the ask question", () => {
@@ -26,4 +26,19 @@ test("renders submit button", () => {
   expect(inputElement).toBeInTheDocument();
   expect(inputElement.getAttribute("type")).toEqual("submit");
   expect(inputElement.getAttribute("value")).toEqual("Ask!");
+});
+
+test("empties the text inputs after submitting", async () => {
+  const { getByTestId } = render(<NewAskForm />);
+  const questionInput = getByTestId("question-input");
+  const askeeInput = getByTestId("askee-input");
+  const submitInput = getByTestId("ask-submit");
+
+  fireEvent.change(questionInput, { target: { value: "Hello"}});
+  fireEvent.change(askeeInput, { target: { value: "Bob"}});
+
+  fireEvent.click(submitInput);
+
+  expect((questionInput as HTMLInputElement).value).toEqual("");
+  expect((askeeInput as HTMLInputElement).value).toEqual("");
 });
--- src/components/NewAskForm.tsx
+++ src/components/NewAskForm.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function NewAskForm() {
+  const [question, setQuestion] = useState("");
+  const [askee, setAskee] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setQuestion("");
+    setAskee("");
+  };
+
   return (
-    <form data-testid="new-ask-form" className="new-ask-form">
+    <form
+      data-testid="new-ask-form"
+      className="new-ask-form"
+      onSubmit={onSubmit}>
+
       <input
         type="text"
         placeholder="What do you want to ask for?"
+        value={question}
+        onChange={e => setQuestion(e.target.value)}
         data-testid="question-input" />
       <input
         type="text"
         placeholder="Who are you asking?"
-        data-testid="askee-input" />
+        value={askee}
+        onChange={e => setAskee(e.target.value)}
+        data-testid="askee-input"/>
       <input
         type="submit"
         value="Ask!"
         data-testid="ask-submit" />
+
     </form>
   );
 }
