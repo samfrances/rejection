d0535fcac3bc220c9310b6ffcc86528e4adb7e56
Sam Frances <sam@samfrances.uk>
2020-02-25 23:41:52 +0000

    Add basic form validation

--- src/components/NewAskForm.test.tsx
+++ src/components/NewAskForm.test.tsx
@@ -2,63 +2,121 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import NewAskForm from "./NewAskForm";
 
+function setup() {
+  const onSubmitAsk = jest.fn();
+  const { getByTestId } = render(<NewAskForm onSubmitAsk={onSubmitAsk} />);
+  const questionInput = () => getByTestId("question-input");
+  const askeeInput = () => getByTestId("askee-input");
+  const submitInput = () => getByTestId("ask-submit");
+  const questionError = () => getByTestId("question-error");
+  const askeeError = () => getByTestId("askee-error");
+
+  return {
+    questionInput,
+    askeeInput,
+    submitInput,
+    onSubmitAsk,
+    questionError,
+    askeeError,
+  };
+}
+
 test("renders an input for the ask question", () => {
-  const { getByTestId } = render(<NewAskForm onSubmitAsk={jest.fn()} />);
-  const inputElement = getByTestId("question-input");
-  expect(inputElement).toBeInTheDocument();
-  expect(inputElement.getAttribute("type")).toEqual("text");
-  expect(inputElement.getAttribute("placeholder"))
+
+  const { questionInput: inputElement } = setup();
+
+  expect(inputElement()).toBeInTheDocument();
+  expect(inputElement().getAttribute("type")).toEqual("text");
+  expect(inputElement().getAttribute("placeholder"))
     .toEqual("What do you want to ask for?");
 });
 
 test("renders an input for the askee", () => {
-  const { getByTestId } = render(<NewAskForm onSubmitAsk={jest.fn()} />);
-  const inputElement = getByTestId("askee-input");
-  expect(inputElement).toBeInTheDocument();
-  expect(inputElement.getAttribute("type")).toEqual("text");
-  expect(inputElement.getAttribute("placeholder"))
+
+  const { askeeInput: inputElement } = setup();
+
+  expect(inputElement()).toBeInTheDocument();
+  expect(inputElement().getAttribute("type")).toEqual("text");
+  expect(inputElement().getAttribute("placeholder"))
     .toEqual("Who are you asking?");
 });
 
 test("renders submit button", () => {
-  const { getByTestId } = render(<NewAskForm onSubmitAsk={jest.fn()} />);
-  const inputElement = getByTestId("ask-submit");
-  expect(inputElement).toBeInTheDocument();
-  expect(inputElement.getAttribute("type")).toEqual("submit");
-  expect(inputElement.getAttribute("value")).toEqual("Ask!");
+
+  const { submitInput: inputElement } = setup();
+
+  expect(inputElement()).toBeInTheDocument();
+  expect(inputElement().getAttribute("type")).toEqual("submit");
+  expect(inputElement().getAttribute("value")).toEqual("Ask!");
 });
 
 test("empties the text inputs after submitting", async () => {
-  const { getByTestId } = render(<NewAskForm onSubmitAsk={jest.fn()} />);
-  const questionInput = getByTestId("question-input");
-  const askeeInput = getByTestId("askee-input");
-  const submitInput = getByTestId("ask-submit");
 
-  fireEvent.change(questionInput, { target: { value: "Hello"}});
-  fireEvent.change(askeeInput, { target: { value: "Bob"}});
+  const { questionInput, askeeInput, submitInput } = setup();
 
-  fireEvent.click(submitInput);
+  fireEvent.change(questionInput(), { target: { value: "Hello"}});
+  fireEvent.change(askeeInput(), { target: { value: "Bob"}});
 
-  expect((questionInput as HTMLInputElement).value).toEqual("");
-  expect((askeeInput as HTMLInputElement).value).toEqual("");
+  fireEvent.click(submitInput());
+
+  expect((questionInput() as HTMLInputElement).value).toEqual("");
+  expect((askeeInput() as HTMLInputElement).value).toEqual("");
 });
 
 test("calls onSubmitAsk callback when form submitted", async () => {
-  const onSubmitAsk = jest.fn();
 
-  const { getByTestId } = render(<NewAskForm onSubmitAsk={onSubmitAsk} />);
-  const questionInput = getByTestId("question-input");
-  const askeeInput = getByTestId("askee-input");
-  const submitInput = getByTestId("ask-submit");
+  const { questionInput, askeeInput, submitInput, onSubmitAsk } = setup();
 
   const ask = "Hello";
   const askee = "Bob";
 
-  fireEvent.change(questionInput, { target: { value: ask }});
-  fireEvent.change(askeeInput, { target: { value: askee }});
+  fireEvent.change(questionInput(), { target: { value: ask }});
+  fireEvent.change(askeeInput(), { target: { value: askee }});
 
-  fireEvent.click(submitInput);
+  fireEvent.click(submitInput());
 
   expect(onSubmitAsk).toHaveBeenCalledWith(ask, askee);
+
+});
+
+test("validates question", async () => {
+
+  const {
+    questionInput, askeeInput, submitInput, onSubmitAsk,
+    questionError
+  } = setup();
+
+  const ask = "Hi";
+  const askee = "Bob";
+
+  fireEvent.change(questionInput(), { target: { value: ask }});
+  fireEvent.change(askeeInput(), { target: { value: askee }});
+
+  fireEvent.click(submitInput());
+
+  expect(onSubmitAsk).not.toHaveBeenCalledWith(ask, askee);
+
+  expect(questionError()).toBeInTheDocument();
+
+});
+
+test("validates question", async () => {
+
+  const {
+    questionInput, askeeInput, submitInput, onSubmitAsk,
+    askeeError
+  } = setup();
+
+  const ask = "Hello";
+  const askee = "Bo";
+
+  fireEvent.change(questionInput(), { target: { value: ask }});
+  fireEvent.change(askeeInput(), { target: { value: askee }});
+
+  fireEvent.click(submitInput());
+
+  expect(onSubmitAsk).not.toHaveBeenCalledWith(ask, askee);
+
+  expect(askeeError()).toBeInTheDocument();
 
 });
--- src/components/NewAskForm.tsx
+++ src/components/NewAskForm.tsx
@@ -7,14 +7,36 @@ interface Props {
 
 export default function NewAskForm({ onSubmitAsk }: Props) {
   const [question, setQuestion] = useState("");
+  const [questionError, setQuestionError] = useState("");
   const [askee, setAskee] = useState("");
+  const [askeeError, setAskeeError] = useState("");
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSubmitAsk(question, askee);
-    setQuestion("");
-    setAskee("");
+    if (validate()) {
+      onSubmitAsk(question, askee);
+      setQuestion("");
+      setAskee("");
+    }
   };
+
+  const validate = () => {
+
+    const questionError =
+      question.length < 3
+        ? "Question must be at least 3 characters long"
+        : ""
+
+    const askeeError =
+      askee.length < 3
+        ? "Askee must be at least 3 characters long"
+        : ""
+
+    setQuestionError(questionError);
+    setAskeeError(askeeError);
+
+    return !(questionError || askeeError);
+  }
 
   return (
     <form
@@ -26,19 +48,22 @@ export default function NewAskForm({ onSubmitAsk }: Props) {
         type="text"
         placeholder="What do you want to ask for?"
         value={question}
-        onChange={e => setQuestion(e.target.value)}
+        onChange={e => { setQuestion(e.target.value); }}
         data-testid="question-input" />
       <input
         type="text"
         placeholder="Who are you asking?"
         value={askee}
-        onChange={e => setAskee(e.target.value)}
+        onChange={e => { setAskee(e.target.value); }}
         data-testid="askee-input"/>
       <input
         type="submit"
         value="Ask!"
         data-testid="ask-submit" />
-
+      <ul className="errors">
+        {questionError ? <li data-testid="question-error">{questionError}</li> : null}
+        {askeeError ? <li data-testid="askee-error">{askeeError}</li> : null}
+      </ul>
     </form>
   );
 }
