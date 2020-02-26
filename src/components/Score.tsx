c8c373d60d7d32dc1a7af778f87ec3748317808c
Sam Frances <sam@samfrances.uk>
2020-02-26 15:58:02 +0000

    Add formatting to score

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "../css/Score.css";
 import { TestIDs } from "./constants";
 
 export default function Score({ score }: { score: number }) {
     return (
-        <div data-testid={TestIDs.Score}>
+        <div data-testid={TestIDs.Score} className="score-container">
             Score: <span className="score">{score}</span>
         </div>
     );
