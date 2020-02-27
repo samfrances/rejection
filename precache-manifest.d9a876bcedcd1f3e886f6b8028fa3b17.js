8bf1b3403b4f295bdfbb621d92732a74071e0ad2
Sam Frances <sam@samfrances.uk>
2020-02-27 22:04:04 +0000

    Create gh-pages branch with static content

---
+++ precache-manifest.d9a876bcedcd1f3e886f6b8028fa3b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68f83a9a5f9df62ee4649b397a6dc50",
+    "url": "/rejection/index.html"
+  },
+  {
+    "revision": "4bce45653f3529151368",
+    "url": "/rejection/static/css/main.b00e04a3.chunk.css"
+  },
+  {
+    "revision": "63b461e348594cabb86b",
+    "url": "/rejection/static/js/2.3329787b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rejection/static/js/2.3329787b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bce45653f3529151368",
+    "url": "/rejection/static/js/main.6da9a657.chunk.js"
+  },
+  {
+    "revision": "f6d084422c6757b29ede",
+    "url": "/rejection/static/js/runtime-main.c45feb6d.js"
+  }
+]);
